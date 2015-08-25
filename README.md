8146386e807e672e60ff933c9bb81fd08a70a716
Tommy Brunn <tommy.brunn@klarna.com>
2015-08-25 14:17:29 +0200

    Add loader and reporter

---
+++ README.md
@@ -0,0 +1,89 @@
+# Less Terrible Coffeelint Loader
+
+This was originally a fork of [coffeelint-loader](https://github.com/bline/coffeelint-loader), but it had some deficiencies. When I tried to fix them, I ended up replacing the whole thing.
+
+I'm using this for an internal project at the company I work for, so don't expect this to work in the general case. While it may technically have support for some features, I have not tested all of them. PRs are welcome, however.
+
+## Usage
+
+Apply the Coffeelint loader as pre/postLoader in your webpack configuration:
+
+``` javascript
+module.exports = {
+	module: {
+		preLoaders: [
+			{
+				test: /\.coffee$/, // include .coffee files
+				exclude: /node_modules/,
+				loader: "less-terrible-coffeelint-loader"
+			}
+		]
+	},
+
+	// more options in the optional coffeelint object
+	coffeelint: {
+		// any coffeelint option http://www.coffeelint.com/#options
+		// i. e.
+		camel_case_classes: 'error',
+
+		// coffeelint errors are displayed by default as warnings
+		// set emitErrors to true to display them as errors
+		emitErrors: false,
+
+		// coffeelint to not interrupt the compilation
+		// if you want any file with coffeelint errors to fail
+		// set failOnErrors to true
+		failOnErrors: false,
+
+        // same as failOnErrors but will throw an exception for
+        // warnings as well
+		failOnWarns: false,
+
+		// custom reporter function
+		reporter: function(errors) {
+            this.emitWarning(errors.map(function(error) {
+                return [
+                    error.lineNumber,
+                    error.message
+                ].join(' ')
+            }).join('\n'));
+        }
+	}
+}
+```
+
+### Custom reporter
+
+By default, `less-terrible-coffeelint-loader` will provide a default reporter.
+
+However, if you prefer a custom reporter, pass a function under the `reporter` key in `coffeelint` options. (see *usage* above)
+
+The reporter function will be passed the array returned from `coffeelint.lint`:
+```js
+[
+    {
+        rule :      'Name of the violated rule',
+        lineNumber: 'Number of the line that caused the violation',
+        level:      'The severity level of the violated rule',
+        message:    'Information about the violated rule',
+        context:    'Optional details about why the rule was violated'
+    }
+]
+```
+
+The reporter function will be excuted with the loader context as `this`. You may emit messages using `this.emitWarning(...)` or `this.emitError(...)`. See [webpack docs on loader context](http://webpack.github.io/docs/loaders.html#loader-context).
+
+The output in the Webpack CLI will usually be:
+```
+...
+
+WARNING in ./path/to/file.js
+<reporter output>
+
+...
+```
+
+## License
+
+MIT (http://www.opensource.org/licenses/mit-license.php)
+
---
+++ index.js
@@ -0,0 +1,136 @@
+'use strict';
+
+var fs              = require('fs');
+var path            = require('path');
+var strip           = require('strip-json-comments');
+var utils           = require('loader-utils');
+var RcFinder        = require('rcfinder');
+var linter          = require('coffeelint').lint;
+var defaultReporter = require('./lib/reporter');
+
+var extend = function (obj) {
+  var source, prop, i, l;
+  for (i = 1, l = arguments.length; i < l; i += 1) {
+    source = arguments[i];
+    for (prop in source) {
+      if (hasOwnProperty.call(source, prop)) {
+        obj[prop] = source[prop];
+      }
+    }
+  }
+  return obj;
+};
+
+var rcFinder = new RcFinder('.coffeelint.json', {
+  loader: function (rcpath) {
+    return rcpath;
+  }
+});
+
+var loadConfigSync = function () {
+  var folder = path.dirname(this.resourcePath);
+  var rcpath = rcFinder.find(folder);
+  if (typeof rcpath !== 'string') {
+    // No .coffeelint.json found.
+    return {};
+  }
+
+  this.addDependency(rcpath);
+  var file = fs.readFileSync(rcpath, 'utf8');
+  return JSON.parse(strip(file));
+};
+
+var loadConfigAsync = function (callback) {
+  var folder = path.dirname(this.resourcePath);
+  rcFinder.find(folder, function (err, rcpath) {
+    if (typeof rcpath !== 'string') {
+      // No .coffeelint.json found.
+      return callback(null, {});
+    }
+
+    this.addDependency(rcpath);
+    fs.readFile(rcpath, 'utf8', function (err, file) {
+      var options;
+
+      if (!err) {
+        try {
+          options = JSON.parse(strip(file));
+        }
+        catch (e) {
+          err = e;
+        }
+      }
+      callback(err, options);
+    });
+  }.bind(this));
+};
+
+
+var checkSource = function (source, config) {
+  // Copy options to own object.
+  var options = this.options.coffeelint;
+  extend(config, options);
+
+  // Copy query to own object.
+  var query = utils.parseQuery(this.query);
+  extend(config, query);
+
+  // Move flags.
+  var emitErrors = config.emitErrors;
+  delete config.emitErrors;
+  var failOnHint = config.failOnHint;
+  delete config.failOnHint;
+
+  // Custom reporter.
+  var reporter = config.reporter;
+  delete config.reporter;
+
+  var errors = linter(source, config);
+  if (errors.length > 0) {
+    if (reporter) {
+      reporter.call(this, errors);
+    } else {
+      var emitter = emitErrors ? this.emitError : this.emitWarning;
+
+      if (emitter) {
+        emitter(defaultReporter(errors));
+      } else {
+        throw new Error('Your module system doesn\'t support emitWarning.' +
+          ' Update available? \n' + defaultReporter(errors));
+      }
+    }
+
+    if (failOnHint && errors.length > 0) {
+      throw new Error('Module failed because of coffeelint error.');
+    }
+  }
+};
+
+
+module.exports = function (source) {
+  this.cacheable();
+
+  var callback = this.async();
+
+  if (!callback) {
+    var config = loadConfigSync.call(this);
+    checkSource.call(this, source, config);
+
+    return source;
+  }
+
+  loadConfigAsync.call(this, function (err, config) {
+    if (err) {
+      return callback(err);
+    }
+
+    try {
+      checkSource.call(this, source, config);
+    }
+    catch (e) {
+      return callback(e);
+    }
+
+    callback(null, source);
+  }.bind(this));
+};
